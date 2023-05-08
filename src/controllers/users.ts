86cd351ec08a2922e1c469731c5cda2c8acdc05a
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 12:43:51 -0500

    feat: add update user endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -26,6 +26,28 @@ export const deleteUser = async (
     const deletedUser = await deleteUserById(id);
 
     return res.json(deletedUser).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { username } = req.body;
+    const { id } = req.params;
+
+    if (!username) return res.sendStatus(400);
+
+    const updatedUser = await getUserById(id);
+
+    updatedUser.username = username;
+    await updatedUser.save();
+
+    return res.status(200).json(updatedUser).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,11 @@
 import crypto from "crypto";
-
-const SECRET = "secret";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (password: string, salt: string) => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
-    .update(SECRET)
+    .update(process.env.SECRET)
     .digest("hex");
 };
