b27a28953a5aa67b3ac21e9c3d84a0bf8c46efd9
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 12:41:57 -0500

    feat: add delete user endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -10,6 +10,22 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     return res.status(200).json(users).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -25,3 +25,23 @@ export const isAuthenticated = async (
     return res.sendStatus(400);
   }
 };
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) return res.sendStatus(403);
+
+    if (currentUserId.toString() !== id) return res.sendStatus(403);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
