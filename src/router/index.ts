e7732030c62d6b4d7549bdf7a2bbb27d892af41b
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 13:00:58 -0500

    feat: add script deploy and default message

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,13 @@ import users from "./users";
 const router = express.Router();
 
 export default (): express.Router => {
+  router.get("/", (req, res) => {
+    res.send({
+      message: "REST API with Express and Typescript",
+      version: "1.0.0",
+      author: "W. Rujel",
+    });
+  });
   authentication(router);
   users(router);
   return router;
