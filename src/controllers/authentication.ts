6a0adfc311d89eabbd76c7bcbef723641a7fcc95
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 12:37:18 -0500

    feat: add user login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,44 @@ import express from "express";
 import { getUserByEmail, createUser } from "../db/users";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.sendStatus(400);
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) return res.sendStatus(400);
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (expectedHash !== user.authentication.password)
+      return res.sendStatus(403);
+
+    const salt = random();
+
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("sessionToken", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { username, email, password } = req.body;
